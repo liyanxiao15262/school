c265874d543f2594260240cb9b3f61f2d07c0325
unknown <>
2017-07-17 18:52:29 +0800

    报名咨询入口 Signed-off-by: unknown <李雁晓>

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import './resize.css';
 import Nav from './nav';
 import Banner from './banner';
+import Inform from './inform';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div><Nav /><Banner/></div>, document.getElementById('root'));
+ReactDOM.render(<div><Nav /><Banner/><Inform/></div>, document.getElementById('root'));
 registerServiceWorker();
