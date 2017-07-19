f324bbcf194308bad8ab05a63807a9abecfac9ef
unknown <>
2017-07-19 15:43:26 +0800

    7.19 Signed-off-by: unknown <李雁晓>

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,13 @@ import Nav from './nav';
 import Banner from './banner';
 import Inform from './inform';
 import Cdio from './CDIO';
+import Introduction from './Introduction';
+import Recommend from './Recommend';
+import Policy from './Policy';
+import Score from './score';
+import Work from './work';
+import Center from './center';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div><Nav /><Banner/><Inform/><Cdio/></div>, document.getElementById('root'));
+ReactDOM.render(<div><Nav /><Banner/><Introduction/><Recommend/><Policy/><Inform/><Cdio/><Score/><Work/><Center/></div>, document.getElementById('root'));
 registerServiceWorker();
