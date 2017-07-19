b049d0c580b269ed7156bb4c5d3bc2e15e62a838
unknown <>
2017-07-19 08:21:23 +0800

    7.19 Signed-off-by: unknown <李雁晓>

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,8 @@ import './resize.css';
 import Nav from './nav';
 import Banner from './banner';
 import Inform from './inform';
+import Cdio from './CDIO';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div><Nav /><Banner/><Inform/></div>, document.getElementById('root'));
+ReactDOM.render(<div><Nav /><Banner/><Inform/><Cdio/></div>, document.getElementById('root'));
 registerServiceWorker();
--- my-app/src/inform.js
+++ my-app/src/inform.js
@@ -5,12 +5,25 @@ import React, { Component } from 'react';
 import './inform.css';
 
 class Inform extends Component {
+    constructor(){
+        super();
+        this.state={
+            tit:"重要通知：",
+            start:"2017年北京石油化工学院招生报名开始啦!",
+            mon:"7",
+            month:"月",
+            day:"19",
+            date:"日",
+            info:"尽快提交报名资料",
+            enter:"报名咨询入口>>"
+        }
+    }
     render(){
         return <div className="lyx_inform">
-                <h3><span>重要通知：</span>2017年北京石油化工学院招生报名开始啦!</h3>
-i               <p className="lyx_time"><span className="lyx_month">7</span>月<span className="lyx_day">19</span>日</p>
-                <p className="lyx_sub">尽快提交报名资料</p>
-                <p className="lyx_en"><a href="" className="lyx_enr">报名咨询入口>></a></p>
+                <h3>{this.state.tit}<span>{this.state.start}</span></h3>
+i               <p className="lyx_time">{this.state.mon}<span className="lyx_month">{this.state.month}</span><span className="lyx_day">{this.state.day}</span>{this.state.date}</p>
+                <p className="lyx_sub">{this.state.info}</p>
+                <p className="lyx_en"><a href="" className="lyx_enr">{this.state.enter}</a></p>
             </div>
     }
 };
